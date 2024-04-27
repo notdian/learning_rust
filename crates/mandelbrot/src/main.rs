37df9881413c14cb4d8a4e5c350ad869d1bd0cea
dianfishekqi <dianfishekqi@gmail.com>
2024-04-27 19:51:09 +0200

    Refactor Complex struct to be generic over T and implement operations for generic types

--- crates/mandelbrot/src/main.rs
+++ crates/mandelbrot/src/main.rs
@@ -6,21 +6,21 @@ use std::{
 };
 
 use image::{png::PNGEncoder, ColorType};
-// TODO: make Complex generic over T where T can be any number type (eg. i32, f32)
+use num::Float;
 #[derive(Copy, Clone, Debug, Default)]
-struct Complex {
-    re: f64,
-    im: f64,
+struct Complex<T: Float = f32> {
+    re: T,
+    im: T,
 }
 
-impl Complex {
+impl<T: Float> Complex<T> {
     #[inline]
-    fn norm_sqr(self) -> f64 {
+    fn norm_sqr(self) -> T {
         self.im.clone() * self.im + self.re * self.re.clone()
     }
 }
 
-impl Add for Complex {
+impl<T: Float> Add for Complex<T> {
     type Output = Self;
     #[inline]
     fn add(self, rhs: Self) -> Self::Output {
@@ -30,9 +30,10 @@ impl Add for Complex {
         }
     }
 }
-impl Sub for Complex {
+impl<T: Float> Sub for Complex<T> {
     type Output = Self;
     #[inline]
+
     fn sub(self, rhs: Self) -> Self::Output {
         Complex {
             re: self.re - rhs.re,
@@ -41,7 +42,7 @@ impl Sub for Complex {
     }
 }
 
-impl Mul for Complex {
+impl<T: Float> Mul for Complex<T> {
     type Output = Self;
     #[inline]
     fn mul(self, rhs: Self) -> Self::Output {
@@ -69,7 +70,7 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    let threads = 8;
+    let threads = num_cpus::get_physical();
     let rows_per_band = bounds.1 / threads + 1; //height / threads + 1
 
     let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
@@ -79,7 +80,7 @@ fn main() {
             let top = rows_per_band * i;
             let height = band.len() / bounds.0;
             let band_bounds = (bounds.0, height);
-            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_upper_left = pixel_to_point::<f32>(bounds, (0, top), upper_left, lower_right);
             let band_lower_right =
                 pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
@@ -133,20 +134,24 @@ fn parse_tuple<T: FromStr>(input: &str, del: &str) -> Option<(T, T)> {
     }
 }
 
-fn pixel_to_point(
-    bounds: (usize, usize),
-    pixel: (usize, usize),
-    upper_left: Complex,
-    lower_right: Complex,
-) -> Complex {
+fn pixel_to_point<T: Float>(
+    (bound_x, bound_y): (usize, usize),
+    (pixel_x, pixel_y): (usize, usize),
+    upper_left: Complex<T>,
+    lower_right: Complex<T>,
+) -> Complex<T> {
     let (width, height) = (
         lower_right.re - upper_left.re,
         upper_left.im - lower_right.im,
     );
 
     Complex {
-        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+        re: upper_left.re
+            + num_traits::cast::<usize, T>(pixel_x).unwrap() * width
+                / num_traits::cast::<usize, T>(bound_x).unwrap(),
+        im: upper_left.im
+            - num_traits::cast::<usize, T>(pixel_y).unwrap() * height
+                / num_traits::cast::<usize, T>(bound_y).unwrap(),
     }
 }
 fn escape_time(c: Complex, limit: u8) -> Option<u8> {
@@ -155,7 +160,7 @@ fn escape_time(c: Complex, limit: u8) -> Option<u8> {
         if z.norm_sqr() > 4.0 {
             return Some(i);
         }
-        z = (z * z) + c
+        z = z * z + c
     }
     None
 }
@@ -164,8 +169,8 @@ fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex, lower_
     assert!(pixels.len() == bounds.0 * bounds.1);
     for row in 0..bounds.1 {
         for col in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (col, row), upper_left, lower_right);
-            pixels[row * bounds.0 + col] = match escape_time(point, 255) {
+            let point = pixel_to_point::<f32>(bounds, (col, row), upper_left, lower_right);
+            pixels[row * bounds.0 as usize + col] = match escape_time(point, 255) {
                 Some(l) => 255 - l,
                 None => 0,
             }
